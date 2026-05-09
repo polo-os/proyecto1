7379a13b0ecfdd7193d5a9bba2e856825e82facf
menco <rmenco134@gmail.com>
2026-05-09 18:55:44 -0500

    Añadiendo ejercicios a el enum.2

--- src/enumm/Ejercicios.java
+++ src/enumm/Ejercicios.java
@@ -4,6 +4,9 @@ public enum Ejercicios {
     press_Banca,pres_Banca_Mancuerna,press_Inclinado,pres_Inlcinado_Mancuerna
     ,pres_Declinado,Peck_deck,apertura_poleas,fondos,Jalón_pecho,remo_Sentado,
     remo_Máquina_Hammer,pull_Overe,dominadas,remo_T,Press_militar,
-    elevaciones_Polea,curl_predicador
+    elevaciones_Polea,curl_predicador,curl_Martillo,extensión_Tríceps,extensión_Encima_Cabeza,
+    prensa,sentadilla_Smith,hack,abductores,extensión_Cuadriceps
+    ,femoral_Acostado,femoral_Sentado,peso_Smith,patada_Glueteo_Polea,hip_Thrust
+    ,pantorrilla_maquina
 
 }
