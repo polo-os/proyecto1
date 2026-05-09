b5be4f51895d489bdbfb312eab7c3a735fb32361
menco <rmenco134@gmail.com>
2026-05-09 18:48:14 -0500

    Añadiendo ejercicios a el enum

--- src/enumm/Ejercicios.java
+++ src/enumm/Ejercicios.java
@@ -1,5 +1,9 @@
 package enumm;
 
 public enum Ejercicios {
-    press_Banca,press_Inclinado,pres_Declinado
+    press_Banca,pres_Banca_Mancuerna,press_Inclinado,pres_Inlcinado_Mancuerna
+    ,pres_Declinado,Peck_deck,apertura_poleas,fondos,Jalón_pecho,remo_Sentado,
+    remo_Máquina_Hammer,pull_Overe,dominadas,remo_T,Press_militar,
+    elevaciones_Polea,curl_predicador
+
 }
