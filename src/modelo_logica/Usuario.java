923738fe5eabcfa9cd9cb4f5774c3feb38da22ac
polo <hayderrojas75@gmail.com>
2026-05-12 17:56:18 -0500

    version 3

--- src/modelo_logica/Usuario.java
+++ src/modelo_logica/Usuario.java
@@ -2,6 +2,11 @@ package modelo_logica;
 
 import Conexion_bd.ConexionBD;
 
+import java.sql.ClientInfoStatus;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Usuario {
     private int id_usuario;
     private String nombre;
@@ -97,7 +102,7 @@ public class Usuario {
 
         this.conexionBD = new ConexionBD();
 
-        String sql = "INSERT INT usuario(nombre,edad,altura,peso,contraseña) VALUES('"+this.nombre+"',"+
+        String sql = "INSERT INTO Usuario(nombre,edad,altura,peso,contraseña) VALUES('"+this.nombre+"',"+
                 this.edad+", "+this.altura+", "+this.peso+", "+this.password+");";
 
         if(this.conexionBD.setAutoCommitBD(false)){
@@ -120,6 +125,98 @@ public class Usuario {
 
     }
 
+    //CRUD READ
+    public List<Usuario> consultarRegistroBD(){
+        List<Usuario> usuarioList = new ArrayList<>();
+
+        this.conexionBD = new ConexionBD();
+
+        String sql = "SELECT * FROM Usuario; ";
+
+        try{
+            ResultSet rs = this.conexionBD.consultaBD(sql);
+            Usuario user;
+
+            while(rs.next()){
+                user = new Usuario();
+
+                user.setId_usuario(rs.getInt("id_usuario"));
+                user.setNombre(rs.getString("nombre"));
+                user.setEdad(rs.getByte("edad"));
+                user.setAltura(rs.getFloat("altura"));
+                user.setPeso(rs.getInt("peso"));
+                user.setPassword(rs.getString("password"));
+                usuarioList.add(user);
+
+            }
+        }
+        catch (Exception e){
+            System.out.println("Error al Consultar "+
+                    "el usuario de la clase usuario.....:"+e+
+                    "\nTipo de Error: "+ e.getMessage());
+        }
+        finally {
+            this.conexionBD.cerrarConexion();
+        }
+        System.out.println("Lista de Usuario :"+usuarioList);
+        return usuarioList;
+    }
+
+    public boolean actualizarRegistroBD(){
+        boolean conf;
+
+        this.conexionBD = new ConexionBD();
+
+        String sql = "UPDATE Usuario SET nombre ='"+this.nombre+"', edad = "+
+                this.edad+", altura = "+this.altura+", peso ="+this.peso+", password = "+this.password+";";
+
+        if(this.conexionBD.setAutoCommitBD(false)){
+            if(this.conexionBD.insertarBD(sql)){
+                this.conexionBD.commitBD();
+                this.conexionBD.cerrarConexion();
+                conf = true;
+            }
+            else {
+                this.conexionBD.rollbackBD();
+                this.conexionBD.cerrarConexion();
+                conf = false;
+            }
+        }
+        else {
+            this.conexionBD.cerrarConexion();
+            conf = false;
+        }
+        return conf;
+
+    }
+
+    public boolean borrarRegistroBD() {
+        boolean conf;
+
+        this.conexionBD = new ConexionBD();
+
+        String sql = "DELETE FROM Usuario WHERE id = " +
+                this.id_usuario + ";";
+
+        if(this.conexionBD.setAutoCommitBD(false)){
+            if(this.conexionBD.insertarBD(sql)){
+                this.conexionBD.commitBD();
+                this.conexionBD.cerrarConexion();
+                conf = true;
+            }
+            else {
+                this.conexionBD.rollbackBD();
+                this.conexionBD.cerrarConexion();
+                conf = false;
+            }
+        }
+        else {
+            this.conexionBD.cerrarConexion();
+            conf = false;
+        }
+        return conf;
+
+    }
 
 }
 
