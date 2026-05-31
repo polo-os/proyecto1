3f5d27e29b010e807330ed45177518de26d38ef9
menco <rmenco134@gmail.com>
2026-05-31 15:49:48 -0500

    FINAL

--- src/enumm/Ejercicios.java
+++ src/enumm/Ejercicios.java
@@ -1,13 +1,13 @@
 package enumm;
 
 public enum Ejercicios {
-    press_Banca,pres_Banca_Mancuerna,press_Inclinado,pres_Inlcinado_Mancuerna
-    ,pres_Declinado,Peck_deck,apertura_poleas,fondos,Jalón_pecho,remo_Sentado,
-    remo_Máquina_Hammer,pull_Overe,dominadas,Pájaros,remo_T,Press_militar,
-    elevaciones_Polea,curl_predicador,curl_Martillo,extensión_Tríceps,extensión_Encima_Cabeza,
-    Patada_tríceps,Curl_muñeca,Hand_grip,Curl_reverso, prensa,sentadilla_Smith,hack,
-    abductor_Maquina,extensión_Cuadriceps
-    ,femoral_Acostado,femoral_Sentado,peso_Smith,patada_Glueteo_Polea,hip_Thrust,Sentadilla_búlgara
-    ,pantorrilla_maquina
+    Press_Banca,pres_Banca_Mancuerna,Press_Inclinado,Pres_Inlcinado_Mancuerna
+    ,Pres_Declinado,Peck_deck,Apertura_poleas,fondos,Jalón_pecho,Remo_Sentado,
+    Remo_Máquina_Hammer,pull_Over,Dominadas,Pájaros,Remo_T,Press_militar,
+    Elevaciones_Laterales_Polea,Curl_predicador,Curl_Martillo,Extensión_Tríceps,extensión_Encima_Cabeza,
+    Patada_tríceps,Rompecráneos,Curl_muñeca,Hand_grip,Curl_reverso, Prensa,sentadilla_Smith,hack,
+    Abductor_Maquina,Extensión_Cuadriceps
+    ,Femoral_Acostado,femoral_Sentado,Peso_Muerto_Smith,Patada_Glueteo_Polea,Hip_Thrust,Sentadilla_búlgara
+    ,Pantorrilla_maquina
 
 }
